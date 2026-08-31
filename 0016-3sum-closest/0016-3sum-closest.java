b5832ef8eacc540c8db96b37a68e1100e5e0e516
varshithachalamalasetty5-maker <varshithachalamalasetty.5@gmail.com>
2026-08-31 19:34:52 +0530

    Time: 17 ms (78.03%), Space: 45.6 MB (49.58%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                // If exact target is found
+                if (sum == target) {
+                    return sum;
+                }
+
+                // Update closest sum
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+
+                // Move pointers
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return closest;
+    }
+}
