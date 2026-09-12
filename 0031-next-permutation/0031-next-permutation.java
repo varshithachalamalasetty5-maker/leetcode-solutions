5834754eb9a03d7b42ffca56961903887e7a0ba6
varshithachalamalasetty5-maker <varshithachalamalasetty.5@gmail.com>
2026-09-12 19:40:41 +0530

    Time: 0 ms (100%), Space: 44.7 MB (51.26%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Find the first decreasing element from the right
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: Find the element just larger than nums[i]
+        if (i >= 0) {
+            int j = n - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            // Swap
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        // Step 3: Reverse the elements after i
+        int left = i + 1;
+        int right = n - 1;
+
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
