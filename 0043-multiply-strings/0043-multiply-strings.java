bd207e2f6425ac35540216076021f74590c43a96
varshithachalamalasetty5-maker <varshithachalamalasetty.5@gmail.com>
2026-09-17 18:49:22 +0530

    Time: 3 ms (77.44%), Space: 43.6 MB (63.55%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+
+        // If either number is 0
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int n = num1.length();
+        int m = num2.length();
+
+        int[] result = new int[n + m];
+
+        // Multiply digit by digit
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int j = m - 1; j >= 0; j--) {
+
+                int digit1 = num1.charAt(i) - '0';
+                int digit2 = num2.charAt(j) - '0';
+
+                int product = digit1 * digit2;
+
+                int pos1 = i + j;
+                int pos2 = i + j + 1;
+
+                int sum = product + result[pos2];
+
+                result[pos2] = sum % 10;
+                result[pos1] += sum / 10;
+            }
+        }
+
+        // Convert array to String
+        StringBuilder answer = new StringBuilder();
+
+        for (int digit : result) {
+
+            // Skip leading zeros
+            if (answer.length() == 0 && digit == 0) {
+                continue;
+            }
+
+            answer.append(digit);
+        }
+
+        return answer.toString();
+    }
+}
