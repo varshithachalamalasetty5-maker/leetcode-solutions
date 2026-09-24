721d9418cb14f1820f64d3234970eef97be1bd62
varshithachalamalasetty5-maker <varshithachalamalasetty.5@gmail.com>
2026-09-24 19:35:00 +0530

    Time: 0 ms (100%), Space: 43.3 MB (90.75%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        // If starting cell has an obstacle
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        dp[0][0] = 1;
+
+        // First column
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                dp[i][0] = dp[i - 1][0];
+            }
+        }
+
+        // First row
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 0) {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        // Fill remaining cells
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
